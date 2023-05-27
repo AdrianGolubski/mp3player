e7f1878393dd3e1bfc586ed30251ea187db0b018
Adrian Golubski <adriangolubski@gmail.com>
2023-05-27 22:34:40 +0200

    UI and backend completed

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,10 +2,10 @@ module mp3player {
     requires javafx.graphics;
     requires javafx.controls;
     requires javafx.fxml;
+    requires jid3lib;
+    requires javafx.media;
 
     exports mp3player.main to javafx.graphics;
     opens mp3player.controller to javafx.fxml;
-
-
-
+    opens mp3player.mp3 to javafx.base;
 }
--- src/main/java/mp3player/controller/MainController.java
+++ src/main/java/mp3player/controller/MainController.java
@@ -1,50 +1,24 @@
 package mp3player.controller;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Menu;
-import javafx.scene.control.MenuItem;
-import javafx.scene.control.Slider;
-import javafx.scene.control.TableView;
-import javafx.scene.control.ToggleButton;
 
 public class MainController {
+    @FXML
+    private ContentPaneController contentPaneController;
 
     @FXML
-    private Menu File;
+    private ControlPaneController controlPaneController;
 
     @FXML
-    private MenuItem aboutMenuItem;
+    MenuPaneController menuPaneController;
 
-    @FXML
-    private MenuItem closeMenuItem;
-
-    @FXML
-    private TableView<?> contentTable;
-
-    @FXML
-    private MenuItem dirMenuItem;
-
-    @FXML
-    private MenuItem fileMenuItem;
-
-    @FXML
-    private Button nextButton;
-
-    @FXML
-    private ToggleButton playButton;
-
-    @FXML
-    private Slider progressSlider;
-
-    @FXML
-    private Button prwviousButton;
-
-    @FXML
-    private Slider volumeSlider;
-
-    public void initialize() {
-        System.out.println("MainController created");
+    public MainController() {
     }
 
+    public void initialize() {
+        System.out.println("Main controller created");
+        System.out.println(controlPaneController);
+        System.out.println(contentPaneController);
+        System.out.println(menuPaneController);
+    }
 }
